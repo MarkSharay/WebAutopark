e9438495b8ad12cc1bb51effeebf2c144fc535aa
Mark Sharay <marc.sharay@gmail.com>
2022-10-25 12:43:08 +0300

    Fixing of project structure

---
+++ Autopark.DAL/Repositories/OrderItemRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Autopark.DAL.Entities;
+
+namespace Autopark.DAL.Repositories
+{
+    internal class OrderItemRepository
+    {
+        string connectionString = null;
+        public OrderItemRepository(string conn)
+        {
+            connectionString = conn;
+        }
+        public List<OrderItem> GetList()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<OrderItem>("SELECT * FROM OrderItems").ToList();
+            }
+        }
+
+        public Component Get(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<Component>("SELECT * FROM OrderItems WHERE OrderItemId = @id", new { id }).FirstOrDefault();
+            }
+        }
+
+        public void Create(OrderItem orderItem)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "INSERT INTO OrderItems " +
+                    "(OrderId, ComponentId, Quantity)" +
+                    " VALUES(@OrderId, @ComponentId, @Quantity)";
+                db.Execute(sqlQuery, orderItem);
+            }
+        }
+
+        public void Update(OrderItem orderItem)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "UPDATE Components SET OrderID = @OrderId, ComponentId = @ComponentId, Quantity = @Quantity" +
+                    "WHERE OrderItemId = @OrderItemId";
+                db.Execute(sqlQuery, orderItem);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "DELETE FROM OrderItems WHERE OrderItemId = @id";
+                db.Execute(sqlQuery, new { id });
+            }
+        }
+    }
+}
